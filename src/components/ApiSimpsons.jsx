b63e2841a84f328ac59f3036892d884dee372165
Federico Orsi <federicoantonio.orsi@gmail.com>
2023-01-11 11:52:48 -0300

    ref

--- src/components/ApiSimpsons.jsx
+++ src/components/ApiSimpsons.jsx
@@ -66,33 +66,18 @@ import { ContextApp } from './Context';
 
          <strong onClick={All}><h1 className='ml-20 bg-slate-900 text-white w-fit rounded-full p-4 hover:cursor-pointer'>ALL</h1></strong>
 
-         <section className='text-center marginL'>
+         
+         
+          
           {filterData.length > 0  ? 
              
              
-             filterData.map(homer =>  <><div className='w-56 flex flex-col justify-center mb-7 border-solid border border-slate-800 pb-4'>
-             <h1 key={homer.id}>{homer.name}</h1>
-             
-             <img className='' src={homer.thumbnailUrl} alt="" />
-             <p className='w'>{homer.description}</p>
-             
-             <div className='flex bg-slate-800'>
-             <h1 className=' bg-slate-800 text-white inline ml-8'>Episodio Nro {homer.episode}</h1>
-             <p className=' bg-slate-800 text-white ml-4'>${homer.Price}</p>
-             </div>
-             <h3 className=' bg-orange-900 text-white pb-1'>Temporada {homer.season}</h3><strong><p>Stock: {homer.Qty} unidades</p></strong>
-             <Link to={`/DetailList/${homer.id}`}><strong><button className='rounded-full btn-accent p-3 text-white mt-2'>Ver Detalle</button></strong></Link>
-             </div>
-             
-
-             </>
-             )
-            
-            : 
-             data.map(homer => <><div className='w-56 flex flex-col justify-center mb-7 border-solid border border-slate-800 pb-4'>
+             filterData.map(homer =>  <>
+             <div className='text-center inline'>
+              <div className='w-56 mb-7 border-solid border border-slate-800 pb-4 m-auto'>
               <h1 key={homer.id}>{homer.name}</h1>
               
-              <img className='' src={homer.thumbnailUrl} alt="" />
+              <img className='inline' src={homer.thumbnailUrl} alt="" />
               <p className='w'>{homer.description}</p>
               
               <div className='flex bg-slate-800'>
@@ -102,12 +87,34 @@ import { ContextApp } from './Context';
               <h3 className=' bg-orange-900 text-white pb-1'>Temporada {homer.season}</h3><strong><p>Stock: {homer.Qty} unidades</p></strong>
               <Link to={`/DetailList/${homer.id}`}><strong><button className='rounded-full btn-accent p-3 text-white mt-2' >Ver Detalle</button></strong></Link>
               </div>
+              </div>
+
+             </>
+             )
+            
+            : 
+             data.map(homer => <>
+              <div className='text-center inline'>
+              <div className='w-56 mb-7 border-solid border border-slate-800 pb-4 m-auto'>
+              <h1 key={homer.id}>{homer.name}</h1>
               
+              <img className='inline' src={homer.thumbnailUrl} alt="" />
+              <p className='w'>{homer.description}</p>
+              
+              <div className='flex bg-slate-800'>
+              <h1 className=' bg-slate-800 text-white inline ml-8'>Episodio Nro {homer.episode}</h1>
+              <p className=' bg-slate-800 text-white ml-4'>${homer.Price}</p>
+              </div>
+              <h3 className=' bg-orange-900 text-white pb-1'>Temporada {homer.season}</h3><strong><p>Stock: {homer.Qty} unidades</p></strong>
+              <Link to={`/DetailList/${homer.id}`}><strong><button className='rounded-full btn-accent p-3 text-white mt-2' >Ver Detalle</button></strong></Link>
+              </div>
+              </div>
 
               </>
               )
             }
-            </section>
+            
+            
              
              
              

58f9ef5a701f7ce3142dd9cc393e32278b37e6ec
Federico Orsi <federicoantonio.orsi@gmail.com>
2023-01-12 17:09:41 -0300

    lassssst

--- src/components/DetailList.jsx
+++ src/components/DetailList.jsx
@@ -77,8 +77,8 @@ const DetailList = () => {
          
 
          <article className="max-[450px]:flex-col-reverse max-[450px]:ml-auto max-[450px]:mt-10  flex justify-center">
-         <div className="max-[450px]:m-auto max-[450px]:mt-20 mr-64">
-         <Link to="/Simpsons"><img src={homerImg} className='max-[450px]:h-[92px] max-[450px]:w-[60px] inline ml-10  mt-3 hover:cursor-pointer w-[45px] h-[65px]'/></Link>
+         <div className="max-[450px]:m-auto max-[450px]:mt-10 mr-64">
+         <Link to="/Simpsons"><img src={homerImg} className='max-[450px]:ml-[30%] max-[450px]:h-[92px] max-[450px]:w-[60px] inline ml-10  mt-3 hover:cursor-pointer w-[45px] h-[65px]'/></Link>
          <p className="max-[450px]:text-sm text-xs ml-3">Haz click en Homero</p>
          <p className="max-[450px]:text-sm text-xs ml-3">para seguir con tu compra...</p>
          </div>

be2aa8d749a3be3123a11cd960c02f1f8b049e65
Federico Orsi <federicoantonio.orsi@gmail.com>
2023-01-20 20:02:46 -0300

    introGif

---
+++ src/components/SimponsIntro.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const SimponsIntro = () => {
+    return (
+        <>
+
+    
+      <iframe className='m-auto' width="560" height="315" src="https://www.youtube.com/embed/aDcFhYtiIEM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+
+        </>
+    );
+};
+
+export default SimponsIntro;
